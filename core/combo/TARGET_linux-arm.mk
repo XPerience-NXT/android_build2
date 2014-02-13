665a4953a4982293935c5c43b71ecd1d47622dcb
klozz (TeamMEX) <xxx.reptar.rawrr.xxx@gmail.com>
2014-02-13 11:56:42 -0600

    Update TARGET_linux-arm.mk

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -68,7 +68,7 @@ endif
 
 TARGET_NO_UNDEFINED_LDFLAGS := -Wl,--no-undefined
 
-TARGET_arm_CFLAGS :=    -O2 \
+TARGET_arm_CFLAGS :=    -O4 \
                         -fomit-frame-pointer \
                         -fstrict-aliasing    \
                         -funswitch-loops
